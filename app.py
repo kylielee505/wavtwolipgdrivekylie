506eed4fb1041270bcb2b08f88bf643cc8fd27f5
Manav Israni <manavisrani07@users.noreply.huggingface.co>
2023-08-17 20:25:45 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import gradio as gr
 from subprocess import call
 
+# UI Components Setup
 with gr.Blocks() as ui:
     with gr.Row():
         video = gr.File(label="Video or Image", info="Filepath of video/image that contains faces to use")
@@ -19,31 +20,35 @@ with gr.Blocks() as ui:
         with gr.Column():
            result = gr.Video()
 
-    def generate(video, audio, checkpoint, no_smooth, resize_factor, pad_top, pad_bottom, pad_left, pad_right):
-        if video is None or audio is None or checkpoint is None:
-            return
+# Define the generate function
+def generate(video, audio, checkpoint, no_smooth, resize_factor, pad_top, pad_bottom, pad_left, pad_right):
+    if video is None or audio is None or checkpoint is None:
+        return
         
-        smooth = "--nosmooth" if no_smooth else ""
+    smooth = "--nosmooth" if no_smooth else ""
         
-        
-        cmd = [
-            "python",
-            "inference.py",
-            "--checkpoint_path", f"checkpoints/{checkpoint}.pth",
-            "--segmentation_path", "checkpoints/face_segmentation.pth",
-            "--enhance_face", "gfpgan",
-            "--face", video.name,
-            "--audio", audio.name,
-            "--outfile", "results/output.mp4",
-        ]
+    cmd = [
+        "python",
+        "inference.py",
+        "--checkpoint_path", f"checkpoints/{checkpoint}.pth",
+        "--segmentation_path", "checkpoints/face_segmentation.pth",
+        "--enhance_face", "gfpgan",
+        "--face", video.name,
+        "--audio", audio.name,
+        "--outfile", "results/output.mp4",
+    ]
 
-        
-        call(cmd)
-        return "results/output.mp4"
+    call(cmd)
+    return "results/output.mp4"
 
-    generate_btn.click(
-        generate, 
-        [video, audio, checkpoint, pad_top, pad_bottom, pad_left, pad_right, resize_factor], 
-        result)
+# Click event for the Generate button
+generate_btn.click(
+    generate, 
+    [video, audio, checkpoint, no_smooth, resize_factor, pad_top, pad_bottom, pad_left, pad_right], 
+    result)
 
-ui.queue().launch(share=True, debug=True, inline=False)
+# Create the interface
+iface = gr.Interface(ui.queue(), "web", debug=True)
+
+# Launch the interface
+iface.launch()
