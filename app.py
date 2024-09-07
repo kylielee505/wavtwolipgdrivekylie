40ce45f45aba41a809905aa4b7285f44f2acdeba
Manav Israni <manavisrani07@users.noreply.huggingface.co>
2024-09-07 05:22:32 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,18 +4,18 @@ from subprocess import call
 
 with gr.Blocks() as ui:
     with gr.Row():
-        video = gr.File(label="Video or Image", info="Filepath of video/image that contains faces to use")
-        audio = gr.File(label="Audio", info="Filepath of video/audio file to use as raw audio source")
+        video = gr.File(label="Video or Image")
+        audio = gr.File(label="Audio")
         with gr.Column():
-            checkpoint = gr.Radio(["wav2lip", "wav2lip_gan"], label="Checkpoint", info="Name of saved checkpoint to load weights from")
-            no_smooth = gr.Checkbox(label="No Smooth", info="Prevent smoothing face detections over a short temporal window")
-            resize_factor = gr.Slider(minimum=1, maximum=4, step=1, label="Resize Factor", info="Reduce the resolution by this factor. Sometimes, best results are obtained at 480p or 720p")
+            checkpoint = gr.Radio(["wav2lip", "wav2lip_gan"], label="Checkpoint")
+            no_smooth = gr.Checkbox(label="No Smooth")
+            resize_factor = gr.Slider(minimum=1, maximum=4, step=1, label="Resize Factor")
     with gr.Row():
         with gr.Column():
-            pad_top = gr.Slider(minimum=0, maximum=50, step=1, value=0, label="Pad Top", info="Padding above")
-            pad_bottom = gr.Slider(minimum=0, maximum=50, step=1, value=10, label="Pad Bottom (Often increasing this to 20 allows chin to be included)", info="Padding below lips")
-            pad_left = gr.Slider(minimum=0, maximum=50, step=1, value=0, label="Pad Left", info="Padding to the left of lips")
-            pad_right = gr.Slider(minimum=0, maximum=50, step=1, value=0, label="Pad Right", info="Padding to the right of lips")
+            pad_top = gr.Slider(minimum=0, maximum=50, step=1, value=0, label="Pad Top")
+            pad_bottom = gr.Slider(minimum=0, maximum=50, step=1, value=10, label="Pad Bottom (Often increasing this to 20 allows chin to be included)")
+            pad_left = gr.Slider(minimum=0, maximum=50, step=1, value=0, label="Pad Left")
+            pad_right = gr.Slider(minimum=0, maximum=50, step=1, value=0, label="Pad Right")
             generate_btn = gr.Button("Generate")
         with gr.Column():
            result = gr.Video()
